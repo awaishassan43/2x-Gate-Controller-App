261feab36ffcf680a94694501962e4484743aa58
Shehar Yar Khan <Sheharyar566@gmail.com>
2021-12-11 01:06:49 +0500

    Updated the relays

--- lib/screens/dashboard/components/device.component.dart
+++ lib/screens/dashboard/components/device.component.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:iot/components/button.component.dart';
+import 'package:iot/components/loader.component.dart';
+import 'package:iot/controllers/device.controller.dart';
 import 'package:iot/enum/route.enum.dart';
 import 'package:iot/models/device.model.dart';
+import 'package:iot/util/functions.util.dart';
 import 'package:iot/util/themes.util.dart';
+import 'package:provider/provider.dart';
 
-class DeviceComponent extends StatelessWidget {
+class DeviceComponent extends StatefulWidget {
   final Device device;
   const DeviceComponent({
     Key? key,
     required this.device,
   }) : super(key: key);
 
+  @override
+  State<DeviceComponent> createState() => _DeviceComponentState();
+}
+
+class _DeviceComponentState extends State<DeviceComponent> {
+  bool isLoading = false;
+
+  Future<void> updateRelayStatus(BuildContext context, String relayID, bool isOpen) async {
+    final DeviceController controller = Provider.of<DeviceController>(context, listen: false);
+    final Device device = controller.devices[widget.device.id]!;
+
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      device.updateDevice('isOpen', isOpen, relayID: relayID);
+      await controller.updateDevice(device);
+
+      showMessage(context, "Controller updated successfully!");
+    } catch (e) {
+      showMessage(context, e.toString());
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   Widget renderRelays() {
     final List<Widget> children = [];
-    final relays = device.relays;
+    final relays = widget.device.relays;
 
     for (int i = 0; i < (relays.length / 2).ceil(); i++) {
       final List<Widget> rowItems = [];
@@ -45,7 +78,9 @@ class DeviceComponent extends StatelessWidget {
                   const SizedBox(height: 2.5),
                   CustomButton(
                     text: isOpen ? "Open" : "Closed",
-                    onPressed: () {},
+                    onPressed: () {
+                      updateRelayStatus(context, relays[j].id, !isOpen);
+                    },
                     backgroundColor: isOpen ? const Color(0xFFfc4646) : const Color(0xFF00e6c3),
                     borderRadius: 7.5,
                     padding: 0,
@@ -73,157 +108,162 @@ class DeviceComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String temperature = device.temperature == null ? '...' : device.temperature!.round().toString();
-    final String humidity = device.humidity == null ? '...' : device.humidity!.toStringAsFixed(1);
-    final String deviceName = device.name;
+    final String temperature = widget.device.temperature == null ? '...' : widget.device.temperature!.round().toString();
+    final String humidity = widget.device.humidity == null ? '...' : widget.device.humidity!.toStringAsFixed(1);
+    final String deviceName = widget.device.name;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: MaterialButton(
-        onPressed: () {
-          Navigator.pushNamed(
-            context,
-            Screen.device,
-            arguments: device,
-          );
-        },
-        padding: const EdgeInsets.all(12.5),
-        color: Colors.white,
-        elevation: 0,
-        highlightElevation: 0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            /**
-             * Top Section
-             */
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: MaterialButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                Screen.device,
+                arguments: widget.device,
+              );
+            },
+            padding: const EdgeInsets.all(12.5),
+            color: Colors.white,
+            elevation: 0,
+            highlightElevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
               children: [
-                Text(
-                  deviceName,
-                  style: const TextStyle(
-                    color: textColor,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                /**
+                 * Top Section
+                 */
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    /**
-                     * Temperature
-                     */
-                    Row(
-                      children: [
-                        Card(
-                          color: Colors.white,
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(3.5),
-                            child: Image.asset(
-                              'assets/icons/temp.png',
-                              width: 18,
-                              height: 18,
-                            ),
-                          ),
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            style: const TextStyle(
-                              color: textColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                            children: [
-                              TextSpan(text: temperature),
-                              const TextSpan(text: "\u00b0"),
-                              const TextSpan(text: "C"),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    /**
-                     * End of temperature
-                     */
-
-                    const SizedBox(width: 5),
-
-                    /**
-                     * Humidity
-                     */
-                    Row(
-                      children: [
-                        Card(
-                          color: Colors.white,
-                          elevation: 5,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(3.5),
-                            child: Image.asset(
-                              'assets/icons/humidity.png',
-                              width: 18,
-                              height: 18,
-                            ),
-                          ),
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            style: const TextStyle(
-                              color: textColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                            children: [
-                              TextSpan(text: humidity),
-                              const TextSpan(text: "%"),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    /**
-                     * End of humidity
-                     */
-
-                    IconButton(
-                      constraints: const BoxConstraints(
-                        maxHeight: 40,
-                        maxWidth: 40,
+                    Text(
+                      deviceName,
+                      style: const TextStyle(
+                        color: textColor,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
                       ),
-                      icon: const Icon(Icons.settings),
-                      onPressed: () {
-                        Navigator.pushNamed(context, Screen.deviceSettings, arguments: device);
-                      },
+                    ),
+                    Row(
+                      children: [
+                        /**
+                         * Temperature
+                         */
+                        Row(
+                          children: [
+                            Card(
+                              color: Colors.white,
+                              elevation: 3,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(3.5),
+                                child: Image.asset(
+                                  'assets/icons/temp.png',
+                                  width: 18,
+                                  height: 18,
+                                ),
+                              ),
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                  color: textColor,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                                children: [
+                                  TextSpan(text: temperature),
+                                  const TextSpan(text: "\u00b0"),
+                                  const TextSpan(text: "C"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        /**
+                         * End of temperature
+                         */
+
+                        const SizedBox(width: 5),
+
+                        /**
+                         * Humidity
+                         */
+                        Row(
+                          children: [
+                            Card(
+                              color: Colors.white,
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(3.5),
+                                child: Image.asset(
+                                  'assets/icons/humidity.png',
+                                  width: 18,
+                                  height: 18,
+                                ),
+                              ),
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                  color: textColor,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                                children: [
+                                  TextSpan(text: humidity),
+                                  const TextSpan(text: "%"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        /**
+                         * End of humidity
+                         */
+
+                        IconButton(
+                          constraints: const BoxConstraints(
+                            maxHeight: 40,
+                            maxWidth: 40,
+                          ),
+                          icon: const Icon(Icons.settings),
+                          onPressed: () {
+                            Navigator.pushNamed(context, Screen.deviceSettings, arguments: widget.device);
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 ),
+                /**
+                 * End of top section
+                 */
+
+                Container(
+                  color: Colors.black12,
+                  height: 0.5,
+                  margin: const EdgeInsets.symmetric(vertical: 7.5),
+                ),
+
+                /**
+                 * Bottom Section
+                 */
+                renderRelays(),
               ],
             ),
-            /**
-             * End of top section
-             */
-
-            Container(
-              color: Colors.black12,
-              height: 0.5,
-              margin: const EdgeInsets.symmetric(vertical: 7.5),
-            ),
-
-            /**
-             * Bottom Section
-             */
-            renderRelays(),
-          ],
+          ),
         ),
-      ),
+        // if (isLoading) const Loader(stretched: false),
+      ],
     );
   }
 }
--- lib/screens/device/device.screen.dart
+++ lib/screens/device/device.screen.dart
@@ -2,15 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:iot/components/error.component.dart';
 import 'package:iot/components/largeButton.component.dart';
+import 'package:iot/components/loader.component.dart';
+import 'package:iot/controllers/device.controller.dart';
 import 'package:iot/controllers/user.controller.dart';
 import 'package:iot/enum/route.enum.dart';
 import 'package:iot/models/device.model.dart';
 import 'package:iot/screens/device/components/item.component.dart';
 import 'package:iot/screens/device/components/sensor.component.dart';
 import 'package:iot/models/relay.model.dart';
+import 'package:iot/util/functions.util.dart';
 import 'package:provider/provider.dart';
 
-class DeviceScreen extends StatelessWidget {
+class DeviceScreen extends StatefulWidget {
   final Device device;
   const DeviceScreen({
     Key? key,
@@ -18,113 +21,149 @@ class DeviceScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<DeviceScreen> createState() => _DeviceScreenState();
+}
+
+class _DeviceScreenState extends State<DeviceScreen> {
+  bool isLoading = false;
+
+  Future<void> updateRelayStatus(BuildContext context, String relayID, bool isOpen) async {
+    final DeviceController controller = Provider.of<DeviceController>(context, listen: false);
+    final Device device = controller.devices[widget.device.id]!;
+
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      device.updateDevice('isOpen', isOpen, relayID: relayID);
+      await controller.updateDevice(device);
+
+      showMessage(context, "Controller updated successfully!");
+    } catch (e) {
+      showMessage(context, e.toString());
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(device.name),
+        title: Text(widget.device.name),
         centerTitle: true,
       ),
-      body: Column(
+      body: Stack(
         children: [
-          /**
-           * Scrollable area
-           */
-          Expanded(
-            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                stream: device.deviceRef!.snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError || snapshot.error != null) {
-                    return ErrorMessage(message: snapshot.error.toString());
-                  }
+          Column(
+            children: [
+              /**
+               * Scrollable area
+               */
+              Expanded(
+                child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                    stream: widget.device.deviceRef!.snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError || snapshot.error != null) {
+                        return ErrorMessage(message: snapshot.error.toString());
+                      }
 
-                  Device tempDevice = device;
+                      Device tempDevice = widget.device;
 
-                  if (snapshot.data != null) {
-                    final Map<String, dynamic> streamData = snapshot.data!.data() as Map<String, dynamic>;
-                    streamData['id'] = tempDevice.id;
+                      if (snapshot.data != null) {
+                        final Map<String, dynamic> streamData = snapshot.data!.data() as Map<String, dynamic>;
+                        streamData['id'] = tempDevice.id;
 
-                    tempDevice = Device.fromMap(streamData, ref: tempDevice.deviceRef);
-                  }
+                        tempDevice = Device.fromMap(streamData, ref: tempDevice.deviceRef);
+                      }
 
-                  final String temperature = tempDevice.temperature == null ? '...' : tempDevice.temperature!.round().toString();
-                  final String humidity = tempDevice.humidity == null ? '...' : tempDevice.humidity!.toStringAsFixed(1);
-                  final List<Relay> relays = tempDevice.relays;
+                      final String temperature = tempDevice.temperature == null ? '...' : tempDevice.temperature!.round().toString();
+                      final String humidity = tempDevice.humidity == null ? '...' : tempDevice.humidity!.toStringAsFixed(1);
+                      final List<Relay> relays = tempDevice.relays;
 
-                  return SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Row(
+                      return SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              DeviceSensor(
-                                sensorName: "Temperature",
-                                value: temperature,
-                                showDegrees: true,
-                                unit: Provider.of<UserController>(context, listen: false).profile!.temperatureUnit,
-                                icon: 'assets/icons/temp.png',
+                              Row(
+                                children: [
+                                  DeviceSensor(
+                                    sensorName: "Temperature",
+                                    value: temperature,
+                                    showDegrees: true,
+                                    unit: Provider.of<UserController>(context, listen: false).profile!.temperatureUnit,
+                                    icon: 'assets/icons/temp.png',
+                                  ),
+                                  const SizedBox(width: 10),
+                                  DeviceSensor(
+                                    sensorName: "Humidity",
+                                    value: humidity,
+                                    showPercent: true,
+                                    icon: 'assets/icons/humidity.png',
+                                  ),
+                                ],
                               ),
-                              const SizedBox(width: 10),
-                              DeviceSensor(
-                                sensorName: "Humidity",
-                                value: humidity,
-                                showPercent: true,
-                                icon: 'assets/icons/humidity.png',
-                              ),
+                              ...relays.map((relay) {
+                                final String name = relay.name;
+                                final bool isOpen = relay.isOpen;
+
+                                return Column(
+                                  children: [
+                                    const SizedBox(height: 30),
+                                    LargeButton(
+                                      icon: isOpen ? Icons.lock_open_rounded : Icons.lock_rounded,
+                                      label: isOpen ? "Opened" : "Closed",
+                                      iconColor: isOpen ? const Color(0xFFfc4646) : const Color(0xFF00e6c3),
+                                      onPressed: () {
+                                        updateRelayStatus(context, relay.id, !isOpen);
+                                      },
+                                      bottomText: name,
+                                    ),
+                                  ],
+                                );
+                              }).toList(),
                             ],
                           ),
-                          ...relays.map((relay) {
-                            final String name = relay.name;
-                            final bool isOpen = relay.isOpen;
+                        ),
+                      );
+                    }),
+              ),
+              /**
+               * End of scrollable area
+               */
 
-                            return Column(
-                              children: [
-                                const SizedBox(height: 30),
-                                LargeButton(
-                                  icon: isOpen ? Icons.lock_open_rounded : Icons.lock_rounded,
-                                  label: isOpen ? "Opened" : "Closed",
-                                  iconColor: isOpen ? const Color(0xFFfc4646) : const Color(0xFF00e6c3),
-                                  onPressed: () {},
-                                  bottomText: name,
-                                ),
-                              ],
-                            );
-                          }).toList(),
-                        ],
-                      ),
+              /**
+               * Bottom Section
+               */
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    BottomSectionItem(text: "History", onPressed: () {}, icon: Icons.history),
+                    BottomSectionItem(text: "Schedule", icon: Icons.history, onPressed: () {}),
+                    BottomSectionItem(
+                      text: "Settings",
+                      icon: Icons.settings,
+                      onPressed: () {
+                        Navigator.pushNamed(context, Screen.deviceSettings, arguments: widget.device);
+                      },
                     ),
-                  );
-                }),
-          ),
-          /**
-           * End of scrollable area
-           */
-
-          /**
-           * Bottom Section
-           */
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                BottomSectionItem(text: "History", onPressed: () {}, icon: Icons.history),
-                BottomSectionItem(text: "Schedule", icon: Icons.history, onPressed: () {}),
-                BottomSectionItem(
-                  text: "Settings",
-                  icon: Icons.settings,
-                  onPressed: () {
-                    Navigator.pushNamed(context, Screen.deviceSettings, arguments: device);
-                  },
+                  ],
                 ),
-              ],
-            ),
+              ),
+              /**
+               * End of bottom section
+               */
+            ],
           ),
-          /**
-           * End of bottom section
-           */
+          if (isLoading) const Loader(message: "Updating controller"),
         ],
       ),
     );
